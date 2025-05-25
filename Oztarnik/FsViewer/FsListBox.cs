7f53476c92d16406e244df8365eaa4b71ad69582
Admin <Admin@LAPTOP-4HRM131V>
2025-05-25 04:00:59 +0300

    asdads

--- Oztarnik/FsViewer/FsListBox.cs
+++ Oztarnik/FsViewer/FsListBox.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -16,6 +18,7 @@ namespace Otzarnik.FsViewer
     public class FsListView : ListView
     {
         private TreeItem _previousItem;
+        private CancellationTokenSource _cancellationTokenSource;
 
         public static readonly DependencyProperty RootProperty =
             DependencyProperty.Register(
@@ -202,33 +205,101 @@ namespace Otzarnik.FsViewer
                 CurrentItem = item;
         }
 
-        public virtual void Search(string searchTerm)
+        public async virtual void Search(string searchTerm) =>
+           await SearchAsync(searchTerm);
+
+        public async Task SearchAsync(string searchTerm)
         {
-            if (string.IsNullOrWhiteSpace(searchTerm))
+            _cancellationTokenSource?.Cancel();
+            _cancellationTokenSource = new CancellationTokenSource();
+            var token = _cancellationTokenSource.Token;
+
+            try
             {
-                Reset();
-                return;
+                if (string.IsNullOrWhiteSpace(searchTerm))
+                {
+                    Reset();
+                    return;
+                }
+
+                var searchTerms = Regex.Split(searchTerm, @"[^\w""]+", RegexOptions.None)
+                        .Where(s => !string.IsNullOrWhiteSpace(s))
+                        .ToArray();
+
+                var items = Root.EnumerateItems().ToList();
+
+                var results = items
+                       .Where(item => searchTerms.All(term => item.Name.Split(' ').Contains(term)));
+
+                if (!results.Any())
+                {
+                    results = items
+                        .Where(item => searchTerms.All(term =>
+                            item.Name.Split(' ')
+                                .Concat((item.Tags ?? Enumerable.Empty<string>()).SelectMany(tag => tag.Split(' ')))
+                                .Contains(term, StringComparer.OrdinalIgnoreCase)))
+                        .OrderByDescending(x => GetScore(x, searchTerms));
+                }
+
+
+                if (!results.Any())
+                    results = items
+                        .Select(item => new { Item = item, Score = GetScore(item, searchTerms) })
+                        .Where(x => x.Score > 0)
+                        .OrderByDescending(x => x.Score)
+                        .Select(x => x.Item)
+                        .Take(100)
+                        .ToList();
+
+                if (results.Any())
+                {
+                    CurrentItem = new TreeItem { Items = new ObservableCollection<TreeItem>() };
+                    foreach (var item in results)
+                    {
+                        await Task.Delay(1, token);
+                        token.ThrowIfCancellationRequested();
+                        await Dispatcher.InvokeAsync(() =>
+                            CurrentItem.AddChild(item));
+                    }
+                }
+            }
+            catch (OperationCanceledException) { }
+        }
+
+        private int GetScore(TreeItem item, string[] terms)
+        {
+            int nameScore = terms.Sum(term =>
+            {
+                if (item.Name == term) return 10;
+                if (item.Name.StartsWith(term)) return 6;
+                if (item.Name.Contains(term)) return 1;
+                return 0;
+            });
+
+            string fullName = string.Join("", item.Tags ?? Enumerable.Empty<string>()) + item.Name;
+            return nameScore + GetOrderedMatchScore(fullName, terms);
+        }
+
+        private int GetOrderedMatchScore(string text, string[] terms)
+        {
+            int index = 0;
+            int score = 0;
+
+            foreach (var term in terms)
+            {
+                int found = text.IndexOf(term, index);
+                if (found >= 0)
+                {
+                    score += 4;
+                    index = found + term.Length;
+                }
+                else
+                {
+                    return 0;
+                }
             }
 
-            searchTerm = searchTerm.Replace("\"", "").Replace("\'", "").Replace("שולחן", "שלחן").TrimEnd('+');
-            var searchTerms = searchTerm.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            IEnumerable<TreeItem> results;
-
-            if (searchTerms.Length == 1 && searchTerms[0].Length < 5)
-            {
-                results = Root.EnumerateItems().Where(c =>
-                    c.Name.StartsWith(searchTerms[0]) ||
-                    (c.Tags != null && c.Tags.Any(t => t.StartsWith(searchTerms[0]))));
-            }
-            else
-            {
-                results = Root.EnumerateItems().Where(entry => searchTerms.All(
-                    term => entry.Name.Contains(term) ||
-                    (entry.Tags != null && entry.Tags.Any(t => t.Contains(term)))));
-            }
-
-            if (results.Any())
-                CurrentItem = new TreeItem { Items = new ObservableCollection<TreeItem>(results) };
+            return score;
         }
     }
 }
--- Oztarnik/FsViewer/HeadersListBox.cs
+++ Oztarnik/FsViewer/HeadersListBox.cs
@@ -12,7 +12,6 @@ namespace Otzarnik.FsViewer
 {
     internal class HeadersListView : FsListView
     {
-        private CancellationTokenSource _cancellationTokenSource;
         public RelayCommand<TreeItem> NavigateCommand => new RelayCommand<TreeItem>(item => Navigate(item));
 
         public virtual void Navigate(TreeItem item)
@@ -20,99 +19,5 @@ namespace Otzarnik.FsViewer
             if (item != null)
                 OnNavigationRequested(item);
         }
-
-        public async override void Search(string searchTerm) =>
-            await SearchAsync(searchTerm);
-
-        public async Task SearchAsync(string searchTerm)
-        {
-            _cancellationTokenSource?.Cancel();
-            _cancellationTokenSource = new CancellationTokenSource();
-            var token = _cancellationTokenSource.Token;
-
-            try
-            {
-                if (string.IsNullOrWhiteSpace(searchTerm))
-                {
-                    Reset();
-                    return;
-                }
-
-                var searchTerms = Regex.Split(searchTerm, @"[^\w""]+", RegexOptions.None)
-                        .Where(s => !string.IsNullOrWhiteSpace(s))
-                        .ToArray();
-
-                var items = Root.EnumerateItems().ToList();
-
-                 var results = items
-                        .Where(item => searchTerms.All(term => item.Name.Split(' ').Contains(term)));
-
-                if (!results.Any())
-                    results = items
-                      .Where(item => searchTerms.All(term =>
-                           item.Name.Split(' ').Concat(item.Tags .SelectMany(tag => tag.Split(' ')))
-                              .Contains(term, StringComparer.OrdinalIgnoreCase)))
-                                .OrderByDescending(x => GetScore(x, searchTerms));
-
-                if (!results.Any())
-                    results = items
-                        .Select(item => new { Item = item, Score = GetScore(item, searchTerms) })
-                        .Where(x => x.Score > 0)
-                        .OrderByDescending(x => x.Score)
-                        .Select(x => x.Item)
-                        .Take(100)
-                        .ToList();
-
-                if (results.Any())
-                {
-                    CurrentItem = new TreeItem { Items = new ObservableCollection<TreeItem>() };
-                    foreach (var item in results)
-                    {
-                        await Task.Delay(1, token);
-                        token.ThrowIfCancellationRequested();
-                        await Dispatcher.InvokeAsync(() =>
-                            CurrentItem.AddChild(item));
-                    }
-                }
-            }
-            catch (OperationCanceledException) { }
-        }
-
-        private int GetScore(TreeItem item, string[] terms)
-        {
-            int nameScore = terms.Sum(term =>
-            {
-                if (item.Name == term) return 10;
-                if (item.Name.StartsWith(term)) return 6;
-                if (item.Name.Contains(term)) return 1;
-                return 0;
-            });
-
-            string fullName = string.Join("", item.Tags) + item.Name;
-            return nameScore + GetOrderedMatchScore(fullName, terms);
-        }
-
-        private int GetOrderedMatchScore(string text, string[] terms)
-        {
-            int index = 0;
-            int score = 0;
-
-            foreach (var term in terms)
-            {
-                int found = text.IndexOf(term, index);
-                if (found >= 0)
-                {
-                    score += 4; 
-                    index = found + term.Length;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-
-            return score;
-        }
-
     }
 }
